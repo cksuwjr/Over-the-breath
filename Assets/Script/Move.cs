9f5785bd1c4ddedd54b9da42b45ce220437b3f5f
차경훈 <cksuwjr@naver.com>
2022-03-12 20:36:18 +0900

    Move GetKeyDown을 GetKey로 변경, 키보드별 방향 전환 및 임시 캐릭터 사진 추가

--- Assets/Script/Move.cs
+++ Assets/Script/Move.cs
@@ -5,22 +5,27 @@ using UnityEngine;
 public class Move : MonoBehaviour
 {
     Rigidbody2D rb;
+    SpriteRenderer sr;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        if (Input.GetKey(KeyCode.LeftArrow))
         {
             rb.velocity = new Vector2(-5, rb.velocity.y);
+            sr.flipX = true;
         }
-        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        else if (Input.GetKey(KeyCode.RightArrow))
         {
             rb.velocity = new Vector2(5, rb.velocity.y);
+            sr.flipX = false;
+
         }
     }
 }
