cbc22e08685e7931d32253cd733971732bb90bd5
차경훈 <cksuwjr@naver.com>
2022-03-15 21:42:33 +0900

    add simple image for player, jump function, player script

---
+++ Assets/Script/Player.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Player : MonoBehaviour
+{
+    float HP { get; set; }
+    float AttackPower { get; set; }
+    float MaxHp { get; set; }
+    float JumpPower { get; set; }
+}
