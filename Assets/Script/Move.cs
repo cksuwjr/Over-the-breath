61a6e973bbc38794323c3e7d1bc7a333091cbd0f
차경훈 <cksuwjr@naver.com>
2022-03-21 00:16:24 +0900

    Add player animation and apply them

--- Assets/Script/Move.cs
+++ Assets/Script/Move.cs
@@ -6,7 +6,7 @@ public class Move : MonoBehaviour
 {
     Rigidbody2D rb;
     SpriteRenderer sr;
-
+    Animator anim;
     Status stat;
     bool isJumping;
     int JumpCount = 2;
@@ -15,6 +15,7 @@ public class Move : MonoBehaviour
     {
         rb = GetComponent<Rigidbody2D>();
         sr = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
         stat = GetComponent<Status>();
     }
 
@@ -25,15 +26,21 @@ public class Move : MonoBehaviour
         {
             rb.velocity = new Vector2(-stat.MoveSpeed, rb.velocity.y);
             sr.flipX = true;
+            anim.SetBool("Idle", false);
+            anim.SetBool("Walk", true);
         }
         else if (Input.GetKey(KeyCode.RightArrow))
         {
             rb.velocity = new Vector2(stat.MoveSpeed, rb.velocity.y);
             sr.flipX = false;
+            anim.SetBool("Idle", false);
+            anim.SetBool("Walk", true);
         }
         else
         {
             rb.velocity = new Vector2(0, rb.velocity.y);
+            anim.SetBool("Idle", true);
+            anim.SetBool("Walk", false);
         }
 
         if (Input.GetKeyDown(KeyCode.UpArrow))
@@ -42,6 +49,7 @@ public class Move : MonoBehaviour
             {
                 rb.velocity = new Vector2(0, 0);
                 rb.AddForce(new Vector2(0, 2) * stat.JumpPower, ForceMode2D.Impulse);
+                anim.SetBool("Jump", true);
                 JumpCount--;
                 Debug.Log(JumpCount);
             }
@@ -54,10 +62,11 @@ public class Move : MonoBehaviour
     }
     void OnCollisionEnter2D(Collision2D col)
     {
-        if (col.transform.name == "Tilemap")
+        if (col.transform.name == "Ground")
         {
             if (JumpCount != 2)
                 JumpCount = 2;
+            anim.SetBool("Jump", false);
         }
     }
 }
