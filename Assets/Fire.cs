92b891f2010192a48f2405f8aaa146b2b9d51fb9
차경훈 <cksuwjr@naver.com>
2022-03-22 22:00:16 +0900

    Apply player attack motion, animation, and Add fire script

---
+++ Assets/Fire.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fire : MonoBehaviour
+{
+    Rigidbody2D rb;
+    SpriteRenderer sr;
+    int direction;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+        if (GameObject.Find("Player").GetComponent<SpriteRenderer>().flipX)
+        {
+            sr.flipX = true;
+            direction = -1;
+        }
+        else
+        {
+            sr.flipX = false;
+            direction = 1;
+        }
+
+        Debug.Log(direction);
+
+        Destroy(gameObject, 0.3f);
+    }
+    void Update()
+    {
+        rb.velocity = new Vector2(direction * 15, 0);
+    }
+}
