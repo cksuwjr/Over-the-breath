9ce17c97c9bf3fb72bd870d7680f25db1d5274cb
차경훈 <cksuwjr@naver.com>
2022-03-29 15:26:13 +0900

    Add player another skin and Add Skill and Apply new Background

---
+++ Assets/iron_SkillFEffect.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class iron_SkillFEffect : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject, 0.3f);
+    }
+
+}
