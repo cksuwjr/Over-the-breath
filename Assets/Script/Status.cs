0bc059df5778c445df1da5622a7b81116ef30c4b
차경훈 <cksuwjr@naver.com>
2022-04-05 20:27:20 +0900

    죽고 난 후 부활시 드래곤모드 초기화되는 버그 수정, 공격 후 멈춰야 할 시간동안 움직임 한 방향으로 일정시간동안 움직이는 버그 수정

--- Assets/Script/Status.cs
+++ Assets/Script/Status.cs
@@ -12,12 +12,15 @@ public class Status : MonoBehaviour
     [SerializeField]
     protected float _movespeed;
     [SerializeField]
+    protected float _basicspeed;
+    [SerializeField]
     protected float _jumppower;
 
     public float MaxHp { get { return _maxhp; } set { _maxhp = value; } }
     public float HP { get { return _hp; } set { _hp = value; } }
     public int AttackPower { get { return _attackpower; } set { _attackpower = value; } }
     public float MoveSpeed { get { return _movespeed; } set { _movespeed = value; } }
+    public float BasicSpeed { get { return _basicspeed; } set { _basicspeed = value; } }
     public float JumpPower { get { return _jumppower; } set { _jumppower = value; } }
 
     private void Start()
@@ -28,17 +31,20 @@ public class Status : MonoBehaviour
             _attackpower = 50;
         if (_maxhp == 0)
             _maxhp = 500f;
-        if(_movespeed == 0)
+        if(_basicspeed == 0)
+            _basicspeed = 5f;
+        if (_movespeed == 0)
             _movespeed = 5f;
-        if(_jumppower == 0)
+        if (_jumppower == 0)
             _jumppower = 5f;
 
     }
-    public void StatInit(float hp, int power, float Maxhp, float Movespeed, float Jumppower)
+    public void StatInit(float hp, int power, float Maxhp, float Basicspeed, float Movespeed, float Jumppower)
     {
         _maxhp = Maxhp;
         _hp = hp;
         _attackpower = power;
+        _basicspeed = Basicspeed;
         _movespeed = Movespeed;
         _jumppower = Jumppower;
     }
@@ -47,7 +53,9 @@ public class Status : MonoBehaviour
         _maxhp = stat.MaxHp;
         _hp = stat.HP;
         _attackpower = stat.AttackPower;
-        _movespeed = stat.MoveSpeed;
+        _basicspeed = stat.BasicSpeed;
+        _movespeed = stat.BasicSpeed;
+        //_movespeed = stat.MoveSpeed;
         _jumppower = stat.JumpPower;
     }
 }
