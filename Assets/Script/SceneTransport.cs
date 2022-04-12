bf3198d2ec159b461cb43a10f986cf774fd435c3
차경훈 <cksuwjr@naver.com>
2022-04-12 20:30:35 +0900

    씬 교체 작업 중, 몬스터AI 수정, 특정 상황시 UI의 시나리오 스토리호출

---
+++ Assets/Script/SceneTransport.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class SceneTransport : MonoBehaviour
+{
+    public string NextScene;
+    public string StartStory;
+    private void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.transform.tag == "Player")
+        {
+            StartCoroutine(NewSceneStart());
+            Debug.Log("코루틴시작");
+            Debug.Log(name);
+        }
+    }
+    IEnumerator NewSceneStart()
+    {
+        AsyncOperation isComplete = SceneManager.LoadSceneAsync(NextScene, LoadSceneMode.Single);// Additive);
+        isComplete.allowSceneActivation = false;
+        while (!isComplete.isDone)
+        {
+            yield return null;
+            Debug.Log(isComplete.progress);
+            if (isComplete.progress >= 0.9f)
+            {
+                isComplete.allowSceneActivation = true;
+                yield return new WaitForSeconds(0.1f);
+                HiNewScene();
+                break;
+            }
+        }
+
+        
+    }
+    void HiNewScene()
+    {
+        SceneManager.SetActiveScene(SceneManager.GetSceneByName(NextScene));
+        if (StartStory != null)
+            GameObject.Find("UI").GetComponent<UIManager>().StartScenario(StartStory);
+        Destroy(gameObject);
+
+    }
+}
