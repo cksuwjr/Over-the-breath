e89b39256909c74f390f12c52cc4bfeb8b1fa89d
차경훈 <cksuwjr@naver.com>
2022-04-19 16:35:19 +0900

    스토리 추가, StoryUI이후 일부 소환된 몬스터들 Rigidbody적용 안되는 버그 수정

--- Assets/Script/StoryScenarioStarter.cs
+++ Assets/Script/StoryScenarioStarter.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class StoryScenarioStarter : MonoBehaviour
 {
     UIManager officialUI;
+    public GameObject SpawnSomeThing;
+    public float SpawnPlusX;
+    public float SpawnPlusY;
+    public string SpawnedDieAndStartStory;
     void Start()
     {
         officialUI = GameObject.Find("UI").GetComponent<UIManager>();
@@ -12,9 +16,17 @@ public class StoryScenarioStarter : MonoBehaviour
     void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.tag == "Player") {
+            if (SpawnSomeThing != null && SpawnSomeThing.tag == "Enemy")
+            {
+                GameObject Spawned = Instantiate(SpawnSomeThing, transform.position + new Vector3(SpawnPlusX, SpawnPlusY, 0), Quaternion.identity);
+                Spawned.GetComponent<EnemyAI>().SetAttackTarget(GameObject.FindGameObjectWithTag("Player"));
+                Spawned.GetComponent<EnemyAI>().DieAndStartStory = SpawnedDieAndStartStory;
+            }
             officialUI.StartCoroutine(officialUI.StartScenario(name,0));
             Destroy(gameObject);
         }
             
     }
+
+
 }
