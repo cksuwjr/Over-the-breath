4b898ffaeaaf6174d35b5cd84fd355d7452841fd
차경훈 <cksuwjr@naver.com>
2022-04-19 16:35:19 +0900

    스토리 추가, StoryUI이후 일부 소환된 몬스터들 Rigidbody적용 안되는 버그 수정

--- Assets/Script/SceneTransport.cs
+++ Assets/Script/SceneTransport.cs
@@ -8,27 +8,30 @@ public class SceneTransport : MonoBehaviour
     public string NextScene;
     public string StartStory;
     public float startdelay;
+
+    GameObject NowVirtualCAM;
+    GameObject NowCamera;
     private void Start()
     {
         DontDestroyOnLoad(gameObject);
+        NowCamera = GameObject.FindGameObjectWithTag("MainCamera");
+        NowVirtualCAM = GameObject.Find("CAM");
+
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.transform.tag == "Player")
-        {
             StartCoroutine(NewSceneStart());
-            Debug.Log("코루틴시작");
-            Debug.Log(name);
-        }
     }
     IEnumerator NewSceneStart()
     {
+        DontDestroyOnLoad(NowCamera);
+        DontDestroyOnLoad(NowVirtualCAM);
         AsyncOperation isComplete = SceneManager.LoadSceneAsync(NextScene, LoadSceneMode.Single);// Additive);
         isComplete.allowSceneActivation = false;
         while (!isComplete.isDone)
         {
             yield return null;
-            Debug.Log(isComplete.progress);
             if (isComplete.progress >= 0.9f)
             {
                 isComplete.allowSceneActivation = true;
@@ -43,14 +46,18 @@ public class SceneTransport : MonoBehaviour
     void HiNewScene()
     {
         SceneManager.SetActiveScene(SceneManager.GetSceneByName(NextScene));
-        GameObject.Find("CAM").GetComponent<CinemachineVirtualCamera>().Follow = GameObject.FindGameObjectWithTag("Player").transform;
+        NowCamera.tag = "Untagged";
+        NowVirtualCAM.name = "BeforeCAM";
+        GameObject.FindGameObjectWithTag("MainCamera").transform.position = NowCamera.transform.position;
+        Destroy(NowCamera);
+        Destroy(NowVirtualCAM);
+        NowCamera = GameObject.FindGameObjectWithTag("MainCamera");
+        NowVirtualCAM = GameObject.Find("CAM");
+        NowVirtualCAM.GetComponent<CinemachineVirtualCamera>().Follow = GameObject.FindGameObjectWithTag("Player").transform;
 
         if (StartStory != null)
             GameObject.Find("UI").GetComponent<UIManager>().StartCoroutine(GameObject.Find("UI").GetComponent<UIManager>().StartScenario(StartStory,startdelay));
-        if (GameObject.Find("Director") != null)
-        {
-            GameObject.Find("Director").GetComponent<TimelineControl>().Play();
-        }
+        
         Destroy(gameObject);
     }
 }
